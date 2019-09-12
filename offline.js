7f68532235451d46cd4ed8821664d87465697480
guruchian <30399464+semicon@users.noreply.github.com>
2019-09-13 00:54:30 +0700

    mole
    mole

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1568310235,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/mole-sheet0.png",
+		"images/mole-sheet1.png",
+		"images/mole-sheet2.png",
+		"images/bg-sheet0.png",
+		"images/hole-sheet0.png",
+		"images/grass-sheet0.png",
+		"images/playbtn-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
